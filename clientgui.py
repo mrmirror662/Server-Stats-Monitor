68fcd8ac95e94d2c8943b39a77eea878179597a6
barath keshav <62153832+mrmirror662@users.noreply.github.com>
2024-01-16 23:39:49 +0530

    Update clientgui.py
    better UI kek

--- clientgui.py
+++ clientgui.py
@@ -1,13 +1,22 @@
+from cProfile import label
+from curses import BUTTON1_DOUBLE_CLICKED
+from decimal import Clamped
+
 from parse import compile
 import socket
 from tkinter import *
 from tkinter import ttk
 from ttkthemes import ThemedTk
+import tkinter 
+import sv_ttk
 
+import threading
+import time
 entries = []
 buttons = []
 lables = []
 
+auto_ping = False
 
 def from_rgb(rgb):
     """translates an rgb tuple of int to a tkinter friendly color code
@@ -64,28 +73,52 @@ def pingServer(f):
         print(load_info)
         out_str = f"{eip} :avg 1 mins:{load_info[0]}, avg 5 mins:{load_info[1]}, avg 15 mins:{load_info[2]}, threads:{load_info[3]}/{load_info[4]}"
         avg = (load_info[0]+load_info[1]*0.25+load_info[2]*0.06)
-        green = 255 - clamp(0, avg*15, 255)
-        red = clamp(0, avg*20, 255)
+        green = 100 - clamp(0, avg*15, 100)
+        red = clamp(0, avg*20, 254)
         lab.config(text=out_str)
         lab.config(background=from_rgb((int(red), int(green), 25)))
+def keep_pinging():
+    while(automatic_ping_checkbox_var.get() == 1):
+        pingServer(1)
+        time.sleep(0.3)
+    
+def start_pinging(f):
+    x = threading.Thread(target=keep_pinging)
+    x.start()
 
 
+f= open("ips",mode='r+',encoding='utf8')
+
+for i in range(4):
+    lineText=f.readline()
+    print(lineText)
+
+f.close()    
 isdd = True
-root = ThemedTk(theme='blue')
+root = tkinter.Tk()
 style = ttk.Style()
+sv_ttk.set_theme("dark")
 
-print(root.get_themes())
-root.configure(bg=from_rgb((10, 10, 30)))
+#print(root.get_themes())
+root.configure(bg=from_rgb((0, 50, 100)))
 root.geometry("600x600")
+root.title( "Server monitor")
+
 buttons.append(
-    ttk.Button(root, text="Add IP", command=lambda: updateIp(root)).pack(padx=2, pady=2))
-
-ping_server_btn = ttk.Button(root, text="Ping servers",
+    ttk.Button(root, text="Add IP", command=lambda: updateIp(root)).pack(padx=2, pady=5))
+ping_frame = ttk.Frame(root)
+ping_frame.pack(padx=10,pady=10)
+ping_server_btn = ttk.Button(ping_frame, text="Ping servers",
                              command=lambda: pingServer(output_frame))
-ping_server_btn.pack(padx=2, pady=2)
+ping_server_btn.pack(side=LEFT,padx=2,pady=2)
+automatic_ping_checkbox_var =  tkinter.IntVar(ping_frame)
 
+automatic_ping_checkbox = ttk.Checkbutton(ping_frame, text="automatic ping",command=lambda: start_pinging(automatic_ping_checkbox_var),variable=automatic_ping_checkbox_var)
+automatic_ping_checkbox.pack(padx=2, pady=2)
 ip_frame = ttk.Frame(root, borderwidth=2)
 ip_frame.pack(padx=20, pady=20)
+
 output_frame = ttk.Frame(root, borderwidth=2)
 output_frame.pack(padx=0, pady=0)
+
 root.mainloop()
